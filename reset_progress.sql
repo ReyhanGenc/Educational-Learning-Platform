9f7304e7c24d1daff677365e4f90c759af2c79ab
Reyhan Genç <132941015+ReyhanGenc@users.noreply.github.com>
2026-02-23 13:24:05 +0300

    Remove SQL files as requested

--- reset_progress.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-BEGIN
-    UPDATE enrollments 
-    SET lesson_progress = '{}'::jsonb, 
-        progress = 0, 
-        completed_lesson_ids = ARRAY[]::uuid[],
-        last_accessed_lesson_id = NULL;
-END $$;
